84cf58baedf1f5b0ad7e53e6fabf318ca700d361
Your Name <you@example.com>
2021-03-18 14:37:59 -0600

    Spring Boot MVC project created

---
+++ DeeperDive/src/main/java/com/skilldistillery/deeperdive/DeeperDiveApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.deeperdive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeperDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeeperDiveApplication.class, args);
+	}
+
+}
---
+++ DeeperDive/src/test/java/com/skilldistillery/deeperdive/DeeperDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.deeperdive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeeperDiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
